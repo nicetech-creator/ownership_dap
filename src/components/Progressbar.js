3585accdbda268cb30d3c5817646fb4630161859
nicetech-creator <toptech20190609@gmail.com>
2022-03-16 11:03:24 +0800

    step progressbar style fix

--- src/components/Progressbar.js
+++ src/components/Progressbar.js
@@ -9,8 +9,8 @@ const Progressbar = ({
       {/* <img src={process.env.PUBLIC_URL + "/Images/bar.svg"} alt="" /> */}
       <div className="w-100">
         <ul className="progressbar">
-          <li className={completedStep >= 1 ? "active" : ""}></li>
-          <li className={completedStep >= 2 ? "active" : ""}></li>
+          <li className={completedStep >= 1 ? "active" : ""} style={{zIndex:22}}></li>
+          <li className={completedStep >= 2 ? "active" : ""} style={{zIndex:11}}></li>
           <li className={completedStep == 3 ? "active" : ""}></li>
         </ul>
       </div>
